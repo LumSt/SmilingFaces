456e4563c488874d7d1ce041ddb1cb708a6367cd
Lum Situ <lumsitu@Lums-MacBook-Pro.local>
2017-03-21 22:55:00 -0700

    format the code and polish rotation

--- SmilingFaces/ViewController.swift
+++ SmilingFaces/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var arrayImageView: UIImageView!
     @IBOutlet weak var trayView: UIView!
     
@@ -28,13 +28,13 @@ class ViewController: UIViewController {
         
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
     }
-
+    
     @IBAction func onTrayPanGesture(_ sender: UIPanGestureRecognizer) {
         // Absolute (x,y) coordinates in parentView
         let location = sender.location(in: view)
@@ -43,9 +43,10 @@ class ViewController: UIViewController {
         
         if velocity.y > 0 {
             trayView.center = traycenterClose
-            arrayImageView.transform = CGAffineTransform.identity.rotated(by: CGFloat(M_PI))
-                //CGAffineTransform(rotationAngle: 360.0 * CGFloat(M_PI) / 360.0)
-                //arrayImageView.transform.rotated(by: 360.0 * CGFloat(M_PI) / 360.0)
+            arrayImageView.transform = CGAffineTransform(rotationAngle: CGFloat(M_PI))
+            //CGAffineTransform.identity.rotated(by: CGFloat(M_PI)) //another way to rotate
+            
+            //arrayImageView.transform.rotated(by: 360.0 * CGFloat(M_PI) / 360.0) //don't know if this work or not
         } else {
             trayView.center = traycenterOpen
             arrayImageView.transform = CGAffineTransform.identity.rotated(by: CGFloat(M_PI) * 2.0)
@@ -54,7 +55,7 @@ class ViewController: UIViewController {
             NSLog("Gesture began at: %@", NSStringFromCGPoint(location));
             
             originalCenter = trayView.center
-
+            
         } else if (sender.state == UIGestureRecognizerState.changed) {
             NSLog("Gesture changed at: %@", NSStringFromCGPoint(location));
             
@@ -67,50 +68,46 @@ class ViewController: UIViewController {
     
     @IBAction func movemoji(_ sender: UIPanGestureRecognizer) {
         
-            let translation = sender.translation(in: view)
+        let translation = sender.translation(in: view)
+        
+        let uimgview = sender.view as! UIImageView
+        
+        if (sender.state == UIGestureRecognizerState.began) {
+            NSLog("Begin");
+            self.newlyCreatedFace = UIImageView.init(image: uimgview.image)
+            self.view.addSubview(self.newlyCreatedFace)
             
-            let uimgview = sender.view as! UIImageView
-
-//        let faceCenter = CGPoint(x:self.newlyCreatedFace.center.x, y:self.newlyCreatedFace.center.y)
+            self.newlyCreatedFace.center = uimgview.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newLyCreatedCenter = newlyCreatedFace.center
             
-//        self.newlyCreatedFace.center = CGPoint(x: faceCenter.x, y: faceCenter.y + trayView.frame.origin.y)
-            if (sender.state == UIGestureRecognizerState.began) {
-                NSLog("Begin");
-                self.newlyCreatedFace = UIImageView.init(image: uimgview.image)
-                self.view.addSubview(self.newlyCreatedFace)
-                
-                self.newlyCreatedFace.center = uimgview.center
-                newlyCreatedFace.center.y += trayView.frame.origin.y
-                newLyCreatedCenter = newlyCreatedFace.center
-                
-                let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didFacePan(sender:)))
-                newlyCreatedFace.isUserInteractionEnabled = true
-                newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
-                
-                let scaleGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(didFaceScale(sender:)))
-                newlyCreatedFace.addGestureRecognizer(scaleGestureRecognizer)
-                
-                let rotateGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(didFaceRotate(sender:)))
-                newlyCreatedFace.addGestureRecognizer(rotateGestureRecognizer)
-                
-                let deleteFaces = UITapGestureRecognizer(target: self, action: #selector(deleteFace(sender:)))
-                deleteFaces.numberOfTapsRequired = 2
-                newlyCreatedFace.addGestureRecognizer(deleteFaces)
-                
-            } else if (sender.state == UIGestureRecognizerState.changed) {
-                NSLog("Changed");
-                self.newlyCreatedFace.center = CGPoint(x:newLyCreatedCenter.x + translation.x, y: newLyCreatedCenter.y + translation.y)
-                
-            } else if (sender.state == UIGestureRecognizerState.ended) {
-                NSLog("Ended");
-                
-                
-            }
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didFacePan(sender:)))
+            newlyCreatedFace.isUserInteractionEnabled = true
+            newlyCreatedFace.addGestureRecognizer(panGestureRecognizer)
+            
+            let scaleGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(didFaceScale(sender:)))
+            newlyCreatedFace.addGestureRecognizer(scaleGestureRecognizer)
+            
+            let rotateGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(didFaceRotate(sender:)))
+            newlyCreatedFace.addGestureRecognizer(rotateGestureRecognizer)
+            
+            let deleteFaces = UITapGestureRecognizer(target: self, action: #selector(deleteFace(sender:)))
+            deleteFaces.numberOfTapsRequired = 2
+            newlyCreatedFace.addGestureRecognizer(deleteFaces)
+            
+        } else if (sender.state == UIGestureRecognizerState.changed) {
+            NSLog("Changed");
+            self.newlyCreatedFace.center = CGPoint(x:newLyCreatedCenter.x + translation.x, y: newLyCreatedCenter.y + translation.y)
+            
+        } else if (sender.state == UIGestureRecognizerState.ended) {
+            NSLog("Ended");
+            
         }
+    }
     
     func didFacePan(sender: UIPanGestureRecognizer) {
-//        let location = sender.location(in: view)
-//        let veloity = sender.velocity(in: view)
+        //        let location = sender.location(in: view)
+        //        let veloity = sender.velocity(in: view)
         let translation = sender.translation(in: view)
         
         if sender.state == .began {
@@ -132,25 +129,25 @@ class ViewController: UIViewController {
     
     func didFaceScale(sender: UIPinchGestureRecognizer) {
         let scale = sender.scale
-//        var velocity = sender.velocity
+        //        var velocity = sender.velocity
         
         self.newlyCreatedFace.transform = CGAffineTransform(scaleX: scale, y: scale)
         
-//        if sender.state == .began {
-//            UIView.animate(withDuration: 0.2, animations: {
-//                print("Scaling")
-//                self.newlyCreatedFace.transform = CGAffineTransform(scaleX: scale, y: scale)
-//            })
-//        } else if sender.state == .changed {
-//            
-//        } else if sender.state == .ended {
-//            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1, y: 1)
-//        }
+        //        if sender.state == .began {
+        //            UIView.animate(withDuration: 0.2, animations: {
+        //                print("Scaling")
+        //                self.newlyCreatedFace.transform = CGAffineTransform(scaleX: scale, y: scale)
+        //            })
+        //        } else if sender.state == .changed {
+        //
+        //        } else if sender.state == .ended {
+        //            self.newlyCreatedFace.transform = CGAffineTransform(scaleX: 1, y: 1)
+        //        }
     }
     
     func didFaceRotate(sender: UIRotationGestureRecognizer) {
         let rotation = sender.rotation
-//        var velocity = sender.velocity
+        //        var velocity = sender.velocity
         print("Going to rotate")
         self.newlyCreatedFace.transform = CGAffineTransform(rotationAngle: 180.0 * rotation / 180.0)
     }
@@ -158,10 +155,10 @@ class ViewController: UIViewController {
     func deleteFace(sender: UITapGestureRecognizer) {
         newlyCreatedFace.removeFromSuperview()
     }
-        
-        
-    }
     
+    
+}
+
 
 
 
